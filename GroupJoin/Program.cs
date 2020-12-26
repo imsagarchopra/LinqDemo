1efbe9dc63b9d48721eb475e3c80b26273023dcb
Sagar Chopra <Chopra.sagar89@outlook.com>
2020-12-26 16:36:49 +0530

    Group Join

---
+++ GroupJoin/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Example1
+            //Group employees by Department.
+
+            var employeesByDepartment = Department.GetAllDepartments().GroupJoin(Employee.GetAllEmployees(), d => d.ID, e => e.DepartmentID,
+                (department, employees) => new { Department = department, Employees = employees });
+
+
+            foreach (var department in employeesByDepartment)
+            {
+                Console.WriteLine(department.Department.Name);
+                foreach (var employee in department.Employees)
+                {
+                    Console.WriteLine(" " + employee.Name);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(".....................................................");
+
+            //...............................................
+            //Using SQL like syntax
+            //...................................................
+
+            from d in Department.GetAllDepartments()
+            join e in Employee.GetAllEmployees()
+            on d.ID equals e.DepartmentID into eGroup
+            select new
+            {
+
+                Department = d,
+                Employees = eGroup
+            }
+            #endregion Example1
+
+            #region Example2
+            #endregion Example2
+
+            Console.Read();
+        }
+    }
+}
