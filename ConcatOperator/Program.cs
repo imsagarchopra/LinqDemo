e5a6e76bf5b258073b134ebab3df53b616dea73e
Sagar Chopra <Chopra.sagar89@outlook.com>
2020-12-27 13:25:06 +0530

    Concat Operator

---
+++ ConcatOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcatOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Concat operator concatenates two sequences into one sequence.
+            int[] numbers1 = { 1, 2, 3 };
+            int[] numbers2 = { 1, 4, 5 };
+
+            var result = numbers1.Concat(numbers2);
+
+            foreach (var v in result)
+            {
+                Console.WriteLine(v);
+            }
+            Console.Read();
+        }
+    }
+}
