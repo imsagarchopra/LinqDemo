1a16e9af6c2da41ac65840a1e45587495111a253
Sagar Chopra <Chopra.sagar89@outlook.com>
2020-12-27 13:23:25 +0530

    Generation Operators

---
+++ GenerationOperators/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerationOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Range
+            IEnumerable<int> evenNumbers = Enumerable.Range(1, 10);
+
+            foreach(int i in evenNumbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("..................................................");
+            #endregion Range
+
+            #region Repeat
+            IEnumerable<string> result = Enumerable.Repeat("Hello", 5);
+
+            foreach(string str in result)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("..................................................");
+            #endregion Repeat
+
+            #region Empty
+            IEnumerable<int> result1 = Enumerable.Empty<int>();
+
+            Console.WriteLine($"Count = {result1.Count()}");
+            #endregion Empty
+
+            Console.Read();
+        }
+    }
+}
---
+++ GenerationOperators/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenerationOperators")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenerationOperators")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a74f759-851c-41a8-94ec-effff3ab5bf4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
